fc9728fc558bc2b00d13119de65fb8c220b0cd77
Alarcon Ace Belen <alarconacebelen@gmail.com>
2022-04-16 16:56:57 +0800

    Format config for better readability

--- code/GUIElements.py
+++ code/GUIElements.py
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from dataclasses import replace
 from os.path import exists
 
 from PyQt5.QtGui import (QIcon, QTransform)
