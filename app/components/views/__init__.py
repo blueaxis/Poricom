671bb228a9b62bd44c5af15afa3bcbc3e0c3e6ba
Alarcon Ace Belen <alarconacebelen@gmail.com>
2023-01-15 00:24:56 +0800

    Rename MainView to WorkspaceView
    Avoid confusion on main app file and main view

--- app/components/views/__init__.py
+++ app/components/views/__init__.py
@@ -17,5 +17,5 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from .main import MainView
+from .workspace import WorkspaceView
 from .ocr import FullScreenOCRView
