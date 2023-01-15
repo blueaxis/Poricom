7c2774852327bb1f752032850a148763c66be630
Alarcon Ace Belen <alarconacebelen@gmail.com>
2023-01-15 16:39:05 +0800

    Move event filters to services

--- app/components/services/filters.py
+++ app/components/services/filters.py
@@ -1,5 +1,6 @@
 """
 Poricom Services
+
 Copyright (C) `2021-2022` `<Alarcon Ace Belen>`
 
 This program is free software: you can redistribute it and/or modify
@@ -16,5 +17,14 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from .filters import WinEventFilter
-from .workers import BaseWorker, BaseWorkerSignal
+from PyQt5.QtCore import (QAbstractNativeEventFilter)
+
+
+class WinEventFilter(QAbstractNativeEventFilter):
+    def __init__(self, keybinder):
+        self.keybinder = keybinder
+        super().__init__()
+
+    def nativeEventFilter(self, eventType, message):
+        ret = self.keybinder.handler(eventType, message)
+        return ret, 0
